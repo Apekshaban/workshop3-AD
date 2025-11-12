60485085aae0080ca724da8196d2220ba7f22cd1
Apeksha-1-hub <meeraban123@gmail.com>
2025-11-12 11:20:44 +0545

    Task2

---
+++ Task2/Null Operations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class NullOperations
+{
+    public void PerformNullChecks()
+    {
+        string username = null;
+
+        // Ternary operator check
+        Console.WriteLine(username == null ? "Username is not available" : username);
+
+        // Null-Coalescing operator
+        Console.WriteLine(username ?? "Username is not available (??)");
+
+        // Null-Coalescing Assignment operator
+        username ??= "DefaultUser";
+        Console.WriteLine("Updated username: " + username);
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NullOperations n = new NullOperations();
+            n.PerformNullChecks();
+        }
+    }
+}
