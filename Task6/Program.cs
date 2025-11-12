8faef884bb238e0a460a4d75387e1789d9f82d8d
Apeksha-1-hub <meeraban123@gmail.com>
2025-11-12 12:03:51 +0545

    six commit

---
+++ Task6/Program.cs
@@ -0,0 +1,51 @@
+﻿
+
+class ErrorHandling
+{
+    public void ConvertInput()
+    {
+        try
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You entered: " + number);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid number format");
+        }
+        finally
+        {
+            Console.WriteLine("Program Executed");
+        }
+    }
+
+    public void CheckPassword()
+    {
+        try
+        {
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+
+            if (password.Length < 6)
+            {
+                throw new Exception("Password must be at least 6 characters.");
+            }
+            else
+            {
+                Console.WriteLine("Password satisfied the requirement");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    static void Main()
+    {
+        ErrorHandling obj = new ErrorHandling();
+        obj.ConvertInput();
+        obj.CheckPassword();
+    }
+}
