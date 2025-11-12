317516abd09634c28ccf9939e1fb84ecdf2d9daa
Apeksha-1-hub <meeraban123@gmail.com>
2025-11-12 11:08:42 +0545

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+   public  static void Main(string[] args)
+    {
+        Operators op = new Operators();
+
+        op.Add(20, 5);
+        op.Subtract(30, 5);
+        op.Multiply(20, 5);
+        op.Divide(10, 5);
+        op.OddEvenFinder(7);
+    }
+}
